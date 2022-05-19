2a298f1b21d38ed17e140099ef3544a9ddbe9879
Philippe Raude <73715187+austral-electronics@users.noreply.github.com>
2022-05-19 17:13:26 +0200

    Add files via upload

---
+++ script/backup_all_config_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#sed -i 's/\r$//' *.sh
+cd ~/git
+#mkdir /get_config
+cp /boot/config.txt ~/git/get_config/config.txt
+cp /etc/dhcpcd.conf ~/git/get_config/dhcpcd.conf
+cp /etc/network/interfaces ~/git/get_config/interfaces
+cp /etc/samba/smb.conf ~/git/get_config/smb.conf
+
